8ac29acb679845c17ccdeb4a5d7b8878a9296f72
debochak <64720227+debochak@users.noreply.github.com>
2021-07-06 20:26:14 +0530

    init

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const { userInfo } = require("os");
+const app = express();
+const port = 3000;
+app.use(express.json());
+
+
+const fs = require("fs")
+const path = "/Users/debo/Documents/Guvi/Node/Task/FileSystem"
+
+var timestamp = new Date().toTimeString();
+var date = new Date().toString();
+
+var filename = path+date;
+console.log(filename);
+
+fs.readdir(path, (err, files)=>{
+    if(err) throw err;
+    console.log(files);
+})
+
+app.post('/files', (req,res)=>{
+    fs.writeFile(date,timestamp,(err)=>{
+        if(err) throw err;
+        res.status(200).send(`new file created with name "${date}" and content "${timestamp}"`)
+    })
+});
+
+app.get('/files', (req,res)=>{
+    fs.readdir(path,(err,files)=>{
+        if(err) throw err;
+        // for (var i in files){
+            // res.write(`<li>${files[i]}</li>`)
+            // res.write("hello")
+            res.status(200).json(files)
+        // }
+    })
+});
+
+app.get('/', (req,res)=>{
+    res.send("This is my home page for my filesystem API")
+});
+
+app.listen((port),()=>{
+    console.log("listening in port ", port)
+})
+
